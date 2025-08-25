6ac5f81914438ed59b94e75d817b50cadbfc96d5
PariyaJ <pariyajafari95@gmail.com>
2025-08-25 15:15:27 +0330

    add-taco-controller

--- src/main/java/ir/bpm/tacocloud/model/TacoOrder.java
+++ src/main/java/ir/bpm/tacocloud/model/TacoOrder.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Data
 public class TacoOrder {
     private String deliveryName;
-    private String deliveryAddress;
+    private String deliveryStreet;
     private String deliveryCity;
     private String deliveryState;
     private String deliveryZip;
@@ -18,7 +18,7 @@ public class TacoOrder {
 
     private List<Taco> tacos = new ArrayList<>();
 
-    private void addTaco(Taco taco) {
+    public void addTaco(Taco taco) {
         this.tacos.add(taco);
     }
 }
---
+++ src/main/java/ir/bpm/tacocloud/util/IngredientByIdConverter.java
@@ -0,0 +1,46 @@
+package ir.bpm.tacocloud.util;
+
+import ir.bpm.tacocloud.model.Ingredient;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class IngredientByIdConverter implements Converter<String, Ingredient> {
+
+    private Map<String, Ingredient> ingredientMap = new HashMap<>();
+
+    public IngredientByIdConverter() {
+        ingredientMap.put("FLTO",
+                new Ingredient("FLTO", "Flour Tortila", Ingredient.Type.WARP));
+        ingredientMap.put("COTO",
+                new Ingredient("COTO", "Corn Tortila", Ingredient.Type.WARP));
+        ingredientMap.put("GRBF",
+                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
+        ingredientMap.put("CARN",
+                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN));
+        ingredientMap.put("TMTO",
+                new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES));
+        ingredientMap.put("LETC",
+                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES));
+        ingredientMap.put("CHED",
+                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE));
+        ingredientMap.put("JACK",
+                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE));
+        ingredientMap.put("SLSA",
+                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE));
+        ingredientMap.put("SRCR",
+                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE));
+    }
+    @Override
+    public Ingredient convert(String id) {
+        return ingredientMap.get(id);
+    }
+
+    @Override
+    public <U> Converter<String, U> andThen(Converter<? super Ingredient, ? extends U> after) {
+        return Converter.super.andThen(after);
+    }
+}
